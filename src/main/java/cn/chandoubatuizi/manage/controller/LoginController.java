b818d8552f4f90c678991610861806a34512c759
chandoubatuizi <425707361@qq.com>
2019-05-07 23:35:06 +0800

    1.解决thymeleaf中文乱码 2.thymeleaf页面测试+shiro测试

--- src/main/java/cn/chandoubatuizi/manage/controller/LoginController.java
+++ src/main/java/cn/chandoubatuizi/manage/controller/LoginController.java
@@ -3,11 +3,27 @@ package cn.chandoubatuizi.manage.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
+    @RequestMapping("/login")
+    public String login() {
+        return "index";
+    }
 
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "121212121212";
+    }
+
+    @RequestMapping("/")
+    public String redirect2Home() {
+        return "index";
+    }
 }
